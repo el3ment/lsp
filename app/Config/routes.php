bc8c977ff28ebcf36049a46839e3ffa79ad2c5ab
robert <robert@63fb4e68-bf31-4eef-be91-eb21767c3a16>
2013-03-31 06:08:24 +0000

    renamed templates for easier user
    git-svn-id: svn+ssh://dev.lonestarpercussion.com/home/bitnami/svn/LoneStarPercussion@32 63fb4e68-bf31-4eef-be91-eb21767c3a16

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -25,8 +25,8 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-	Router::connect('/assets', array('controller' => 'pages', 'action' => 'display', 'assets'));
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'complete'));
+	Router::connect('/assets', array('controller' => 'pages', 'action' => 'display', 'components'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
