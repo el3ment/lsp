7dc899d698a11f655c7d058cfaaf448544e37f37
robert <robert@63fb4e68-bf31-4eef-be91-eb21767c3a16>
2013-05-16 15:44:02 +0000

    prep for merge
    git-svn-id: svn+ssh://dev.lonestarpercussion.com/home/bitnami/svn/LoneStarPercussion@105 63fb4e68-bf31-4eef-be91-eb21767c3a16

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,49 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    jshint: {
+    browserstack_list: {
+      dev: {
+        username: 'rpottorff@gmail.com',
+        password: 'hash4790!@#'
+      }
+    },
+    browserstack: {
+      dev: {
+        credentials: {
+          username: 'rpottorff@gmail.com',
+          password: 'hash4790!@#'
+        },
+        // optional tunnel configuration - if omitted a tunnel is not started
+        //tunnel: {
+          // your BrowserStack API key
+          // key: 'AgbPRotp3pFOX58Cyu50',
+          // a list of hostnames and ports to expose
+          //hosts: [{
+          //  name: 'HOSTNAME',
+          //  port: PORT,
+          //  sslFlag: 0
+          //}]
+        //},
+        // required worker start configuration
+        start: {
+          // time to wait for workers to start running
+          //queueTimeout: QUEUE_TIMEOUT,
+          // default URL for started workers
+          url: 'http://google.com',
+          // default timeout for started workers
+          //timeout: TIMEOUT,
+          // list of browser types to start, as returned from the list function
+          browsers: [{ version: '4.0', device: 'iPhone 4', os: 'ios' },{ version: '20.0', browser: 'firefox', os: 'mac' }]
+          }
+        }
+      },
+      browserstack_clean: {
+        dev: {
+          username: 'rpottorff@gmail.com',
+          password: 'hash4790!@#'
+        }
+      },
+      jshint: {
       options: {
         curly: true,
         eqeqeq: true,
@@ -43,7 +85,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserstack');
 
   // Default task(s).
-  //grunt.registerTask('default', ['browserstack_list', 'browserstack']);
-  grunt.registerTask('default', ['jshint:lonestarpercussion']);
+  grunt.registerTask('default', ['browserstack_list', 'browserstack', 'browserstack_clean']);
+  //grunt.registerTask('default', ['jshint:lonestarpercussion']);
 
 };
