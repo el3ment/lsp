36aceecc168f8aea3250215c4b64516491aed772
robert <rpottorff@gmail.com>
2013-07-24 20:27:27 -0500

    nfs changes

--- app/Config/Schema/i18n.php
+++ app/Config/Schema/i18n.php
@@ -18,7 +18,7 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-
+ 
 /*
  *
  * Using the Schema command line utility
