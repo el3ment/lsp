db6f457847f4278607191c10acf4de87696eaef4
robert <rpottorff@gmail.com>
2013-07-28 18:20:19 -0500

    changes

--- app/webroot/js/controllers/search.js
+++ app/webroot/js/controllers/search.js
@@ -98,7 +98,8 @@
 
 					onSearch : function(e, data){
 
-						var query = $('input[name="searchQuery"]').val();
+						var query = $('input[name="searchQuery"]').val().trigger('blur');
+						$('.mobileSearch .b3').click();
 
 						// If the query has text, or if there are searched keywords (even if the query is blank - they must be clearing it)
 						if(query !== 'undefined' && query.length || (_state.keywords && (_state.keywords || {}).length)){
@@ -112,6 +113,7 @@
 													 // but search is unique because it's ubiquious and can be done from anywhere
 													 // we might need to redirect (via onBeforeAPICall) and we'll update the state string a little early
 							_this.search(query);
+
 						}
 					},
 
