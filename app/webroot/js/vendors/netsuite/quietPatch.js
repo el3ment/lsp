db9186165edf00a565b1640dbec728b79d2a8d5c
robert <rpottorff@gmail.com>
2013-08-06 17:26:38 -0500

    moved quietpatch

--- app/webroot/js/vendors/netsuite/quietPatch.js
+++ app/webroot/js/vendors/netsuite/quietPatch.js
@@ -1,9 +1,9 @@
-window.setWindowChanged = window.setWindowChanged || $.noop;
-window.validate_field = window.validate_field || $.noop;
-window.selectAndFocusField = window.selectAndFocusField || $.noop;
-window.page_init = window.page_init || $.noop;
-window.process_currency_field_value = window.process_currency_field_value || $.noop;
+window.setWindowChanged = window.setWindowChanged || function(){};
+window.validate_field = window.validate_field || function(){};
+window.selectAndFocusField = window.selectAndFocusField || function(){};
+window.page_init = window.page_init || function(){};
+window.process_currency_field_value = window.process_currency_field_value || function(){};
 
 window.console = window.console || {
 	log: function(){ }
-}
+};
