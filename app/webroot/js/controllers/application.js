6714deb0b045f4edc6d727b5c0faebb622b5f9a1
Robert Pottorff <rpottorff@gmail.com>
2014-05-13 16:44:19 -0600

    testing release 2.0

--- app/webroot/js/controllers/application.js
+++ app/webroot/js/controllers/application.js
@@ -141,6 +141,17 @@
 
 						requestNextScript();
 
+						// Orientation Change Zoom Bug (Bug WR935)
+						if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+						  var viewportmeta = document.querySelector('meta[name="viewport"]');
+						  if (viewportmeta) {
+							viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+							document.body.addEventListener('gesturestart', function() {
+							  viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+							}, false);
+						  }
+						}
+
 					},
 
 					// onAfterStateChange : function(e, data){
